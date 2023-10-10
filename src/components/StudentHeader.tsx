360125260bf7ec79d08e3dd65157f3502ae18f64
erickajanemacatol <erickajanemacs@gmail.com>
2023-10-10 18:59:29 +0800

    Responsive Header for Student Side

--- src/components/StudentHeader.tsx
+++ src/components/StudentHeader.tsx
@@ -4,12 +4,16 @@ import {
     IonToolbar, IonLabel,
     IonHeader,
     IonIcon,
+    IonText,
 } from "@ionic/react";
 import './StudentHeader.css';
 import { personCircle } from "ionicons/icons";
 import React, { useState, useEffect } from "react";
+import { useMediaQuery } from 'react-responsive'
 
-const StudentHeader: React.FC= () => {
+const StudentHeader: React.FC = () => {
+    const isDesktop = useMediaQuery({ minWidth: 992 })
+
     const [activePage, setActivePage] = useState(''); // State to track the active page
 
     useEffect(() => {
@@ -34,46 +38,101 @@ const StudentHeader: React.FC= () => {
     };
 
     return (
-        <IonHeader>
-            <IonToolbar>
-                <div className="header-display">
-                    <div className="title-and-logo">
-                        <IonButton className="logo-btn-size" fill="clear">
-                            <IonImg src="/src/imgs/logo.png"></IonImg>
-                        </IonButton>
-                        <IonLabel className="title-label" color={"light"}>Lipa City Science Integrated National High School Portal</IonLabel>
-                    </div>
-                    <div className="tabs-display">
-                        <IonButton fill="clear" className="tab-btn"
-                            color={"light"}
-                            style={getButtonStyle('home')}
-                            href="/home">Home
-                        </IonButton>
+        <div>
+            {isDesktop ?
+                <><IonHeader>
+                    <IonToolbar>
+                        <div className="header-display">
+                            <div className="title-and-logo">
+                                <IonButton className="logo-btn-size" fill="clear">
+                                    <IonImg src="/src/imgs/logo.png"></IonImg>
+                                </IonButton>
+                                <IonLabel className="title-label" color={"light"}>Lipa City Science Integrated National High School Portal</IonLabel>
+                            </div>
+                            <div className="tabs-display">
+                                <IonButton fill="clear" className="tab-btn"
+                                    color={"light"}
+                                    style={getButtonStyle('home')}
+                                    href="/home">Home
+                                </IonButton>
 
-                        <IonButton fill="clear" className="tab-btn"
-                            color={"light"} href="/activities"
-                            style={getButtonStyle('activities')}>Activities
-                        </IonButton>
+                                <IonButton fill="clear" className="tab-btn"
+                                    color={"light"} href="/activities"
+                                    style={getButtonStyle('activities')}>Activities
+                                </IonButton>
 
-                        <IonButton fill="clear" className="tab-btn"
-                            color={"light"} href="/attendance"
-                            style={getButtonStyle('attendance')}>Attendance
-                        </IonButton>
+                                <IonButton fill="clear" className="tab-btn"
+                                    color={"light"} href="/attendance"
+                                    style={getButtonStyle('attendance')}>Attendance
+                                </IonButton>
 
-                        <IonButton fill="clear" className="tab-btn"
-                            color={"light"} href="/grades"
-                            style={getButtonStyle('grades')}>Grades
-                        </IonButton>
-                    </div>
-                    <div className="profile-btn-pos">
-                        <IonButton fill="clear" color={"light"} size="large" href="/profile">
-                            <IonIcon icon={personCircle} size="large" ></IonIcon>
-                        </IonButton>
-                    </div>
-                </div>
-            </IonToolbar>
-        </IonHeader>
+                                <IonButton fill="clear" className="tab-btn"
+                                    color={"light"} href="/grades"
+                                    style={getButtonStyle('grades')}>Grades
+                                </IonButton>
+                            </div>
+                            <div className="profile-btn-pos">
+                                <IonButton fill="clear" color={"light"} size="large" href="/profile">
+                                    <IonIcon icon={personCircle} size="large" ></IonIcon>
+                                </IonButton>
+                            </div>
+                        </div>
+                    </IonToolbar>
+                </IonHeader></>
+                :
+                /*MOBILE VIEW*/
+                <><IonHeader>
+                    <IonToolbar>
+                        <div className="m-header-display">
+                            <div className="top-head">
+                                <div className="m-title-and-logo">
+                                    <IonButton className="m-logo-btn-size" fill="clear" href="/home" size="default">
+                                        <IonImg src="/src/imgs/logo.png"></IonImg>
+                                    </IonButton>
+                                    <IonText className="m-title-label" color={"light"}>LCSINHS Portal</IonText>
+                                </div>
+
+                                <div className="m-profile-btn-pos">
+                                    <IonButton fill="clear" color={"light"} size="large" href="/profile">
+                                        <IonIcon icon={personCircle} size="large" ></IonIcon>
+                                    </IonButton>
+                                </div>
+                            </div>
+
+
+                            <div className="m-tabs-display">
+                                <IonButton fill="clear" className="m-tab-btn"
+                                    color={"light"}
+                                    style={getButtonStyle('home')}
+                                    href="/home">Home
+                                </IonButton>
+
+                                <IonButton fill="clear" className="m-tab-btn"
+                                    color={"light"} href="/activities"
+                                    style={getButtonStyle('activities')}>Activities
+                                </IonButton>
+
+                                <IonButton fill="clear" className="m-tab-btn"
+                                    color={"light"} href="/attendance"
+                                    style={getButtonStyle('attendance')}>Attendance
+                                </IonButton>
+
+                                <IonButton fill="clear" className="m-tab-btn"
+                                    color={"light"} href="/grades"
+                                    style={getButtonStyle('grades')}>Grades
+                                </IonButton>
+                            </div>
+
+
+                        </div>
+                    </IonToolbar>
+                </IonHeader></>
+            }
+        </div>
+
     );
+
+
 };
 
 
