ba38968f481247de197db807f943be891b5f604f
erickajanemacatol <erickajanemacs@gmail.com>
2023-10-12 15:12:54 +0800

    Responsive Attendance Page

--- src/pages/Home/Attendance/Attendance.tsx
+++ src/pages/Home/Attendance/Attendance.tsx
@@ -1,21 +1,39 @@
-import { IonCard, IonCardContent, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonLabel, IonPage, IonToolbar } from "@ionic/react";
+import { IonButton, IonCard, IonCardContent, IonCheckbox, IonChip, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonToolbar } from "@ionic/react";
 import Header from "../../../components/StudentHeader";
+import './Attendance.css'
+import { addSharp, ellipse, create, trash, add } from "ionicons/icons";
+import { useMediaQuery } from "react-responsive";
 
 const Attendance = () => {
+    const isDesktop = useMediaQuery({ minWidth: 992 })
+
     return (
+
         <IonPage>
-            <IonHeader>
-                <IonToolbar class="toolbar-height">
-                    <Header/>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent color={'dark'} scrollX={false}>
-                <IonCard className="my-grades-card">
-                    <IonCardContent>
-                        <IonLabel className="my-activity-text">My Classes</IonLabel>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
+            <Header />
+
+            {isDesktop ? <>
+                <IonContent color={'light'} scrollX={false}>
+                    <div className="spacer-h-m" />
+                    <div className="display-flex">
+
+                        <IonLabel className="my-att-title">My Classes </IonLabel>
+                    </div>
+                </IonContent>
+            </>
+                :
+
+                /*MOBILE VIEW*/
+                <>
+                    <IonContent color={'light'} scrollX={false}>
+                    <div className="spacer-h-s" />
+                    <div className="display-flex">
+                        <IonLabel className="m-my-att-title">My Classes </IonLabel>
+                    </div>
+                </IonContent>
+                </>
+
+            }
         </IonPage>
     );
 };
